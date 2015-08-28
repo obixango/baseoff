895ea1fd880759e0ffda053318bd5a9a2e3f8b25
dandre <obixango@gmail.com>
2015-08-28 18:42:44 +0000

    add aws3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,19 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { :host => 'http://baseoff.herokuapp.com/' }
+  
+  # config/initializers/paperclip.rb
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+
+  
+  #Sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
 end
